8c37e6a43c3185805dc17fee8e5acfb50c377038
Nicholas Becker <Nicholas.becker@students.dominican.edu>
2019-04-26 11:16:30 -0700

    Users can see all mock events

--- app.js
+++ app.js
@@ -9,12 +9,27 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // Use handlebars to render
 app.set('view engine', 'handlebars');
+var events = [
+  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+]
 
 // Tell our app to send the "hello world" message to our home page
 
 app.get('/', (req, res) => {
   res.render('home', { msg: 'Handlebars are Cool!' });
 });
+
+
+// INDEX
+app.get('/', (req, res) => {
+  res.render('events-index', { events: events });
+})
+
+app.get('/events', (req, res) => {
+  res.render('events-index', { events: events });
+})
 
 
 // Render the "home" layout for the main page and send the following msg
