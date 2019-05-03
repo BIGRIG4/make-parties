1228ae7c28101d25ac87e02d5d5d893cf8d91a6a
Nicholas Becker <Nicholas.becker@students.dominican.edu>
2019-05-03 10:10:45 -0700

    Users can create events

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 // Initialize express
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+
+
+
+
+
 
 // require handlebars
 var exphbs = require('express-handlebars');
 
 // Use "main" as our default layout
+app.use(bodyParser.urlencoded({ extended: true }));
+const models = require('./db/models');
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // Use handlebars to render
 app.set('view engine', 'handlebars');
@@ -14,12 +24,31 @@ var events = [
   { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
   { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
 ]
+app.post('/events', (req, res) => {
+  console.log(req.body);
+})
+
+app.post('/events', (req, res) => {
+  models.Event.create(req.body).then(event => {
+    res.redirect(`/`);
+  }).catch((err) => {
+    console.log(err)
+  });
+})
 
 // Tell our app to send the "hello world" message to our home page
 
 app.get('/', (req, res) => {
   res.render('home', { msg: 'Handlebars are Cool!' });
 });
+
+app.get('/', (req, res) => {
+  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+    res.render('events-index', { events: events });
+  })
+})
+
+
 
 
 // INDEX
@@ -29,6 +58,10 @@ app.get('/', (req, res) => {
 
 app.get('/events', (req, res) => {
   res.render('events-index', { events: events });
+})
+
+app.get('/events/new', (req, res) => {
+  res.render('events-new', {});
 })
 
 
